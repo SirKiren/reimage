79e894a0f1a86a127893e2e132e134c84fef3e10
SirKiren <82477325+SirKiren@users.noreply.github.com>
2024-05-13 18:33:02 -0400

    fix surface initramfs

--- config/scripts/initramfs.sh
+++ config/scripts/initramfs.sh
@@ -5,5 +5,14 @@ plymouth-set-default-theme details
 set -euo pipefail
 
 rpm-ostree cliwrap install-to-root /
-QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//')"
+
+if [[ "${KERNEL_FLAVOR}" == "surface" ]]; then
+    KERNEL_SUFFIX="surface"
+else
+    KERNEL_SUFFIX=""
+fi
+
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
+
+#QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//')"
 /usr/libexec/rpm-ostree/wrapped/dracut --no-hostonly --kver "${QUALIFIED_KERNEL}" --reproducible -v --add ostree -f "/lib/modules/${QUALIFIED_KERNEL}/initramfs.img"
