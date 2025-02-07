47832df6ade0318a89dd40121d36d4cfc5e07fdc
SirKiren <82477325+SirKiren@users.noreply.github.com>
2025-02-07 16:29:50 -0500

    repo cleanup

--- Archive/files/scripts/initramfs-surface.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# mkdir -p /var/tmp mkdir -p /var/roothome
-plymouth-set-default-theme details
-set -euo pipefail
-
-rpm-ostree cliwrap install-to-root /
-QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-surface-(\d+\.\d+\.\d+)' | sed -E 's/kernel-surface-//')"
-/usr/libexec/rpm-ostree/wrapped/dracut --no-hostonly --kver "${QUALIFIED_KERNEL}" --reproducible -v --add ostree -f "/lib/modules/${QUALIFIED_KERNEL}/initramfs.img"
